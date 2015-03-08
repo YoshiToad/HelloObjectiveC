3c27125c35cf866db6365ba0a473cb808d41bc86
Peter Hui <peterhui@Peters-MacBook-Pro.local>
2015-03-08 14:40:23 +0800

    Initial Commit

---
+++ HelloObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by Peter Hui on 8/3/15.
+//  Copyright (c) 2015 Peter Hui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
